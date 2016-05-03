2282aa5365c033183065787aec8dae5e1eef07ff
anu.arya <aarya@cainc.com>
2016-05-02 22:48:06 -0400

    added JsonObject

--- src/test/java/Example1Test.java
+++ src/test/java/Example1Test.java
@@ -1,17 +1,17 @@
 import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 //import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
-import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
+
+import net.minidev.json.JSONObject;
 
 public class Example1Test {
 
@@ -24,6 +24,10 @@ public class Example1Test {
 	private static String schoolALink;
 	private static String schoolBLink;
 	private static String onboardId;
+	private static String elaProductId;
+	private static String mathProductId;
+	JSONObject jsonObj = new JSONObject();
+	private static String jsonString;
 
 	@Before
 	public void setup() {
@@ -31,10 +35,17 @@ public class Example1Test {
 
 	}
 
+	
+	// get authentication token
 	@Test
 	public void T01_test() {
+		jsonObj.put("username", "DisAdmin");
+		jsonObj.put("password", "password");
+		jsonObj.put("state", "CO");
+		
+		jsonString = jsonObj.toString();
 		res = given().contentType(ContentType.JSON)
-				.body("{ \"username\": \"DisAdmin\", \"password\": \"password\", \"state\": \"CO\" }\"}").when()
+				.body(jsonString).when()
 				.post("/login/oauth/user_token");
 		json = RestUtil.getJsonPath(res);
 		Assert.assertEquals(res.statusCode(), 200);
@@ -44,6 +55,8 @@ public class Example1Test {
 		System.out.println(res.statusCode());
 	}
 
+	
+	//get all schools in the district
 	@Test
 	public void T02_test() {
 		RestUtil.setBasePath("/api/v1/schools");
@@ -60,10 +73,16 @@ public class Example1Test {
 		System.out.println(onboardId);
 	}
 	
+	//get all licenses(products)for the district
 	@Test
 	public void T03_test(){
-		RestUtil.setBasePath("");
-		
+		RestUtil.setBasePath("/api/v1/products");
+		res = given().header("Content_Type", "Application/json").and()
+				.header("Authorization", tokenType + " " + accessToken).when().get();
+		json = RestUtil.getJsonPath(res);
+		Assert.assertEquals(res.statusCode(), 200);
+		json = new JsonPath(res.asString());
+		elaProductId = json.getString("data.items[0].id");		
 	}
 
 	@After
